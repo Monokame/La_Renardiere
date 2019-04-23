e68c9f2adc56608d96ea40d1b9972bc9a4e3d5bb
Monokame <jeremy.marc.pro@gmail.com>
2019-04-23 17:54:15 +0200

    envoie vers web service

--- action.php
+++ action.php
@@ -2,22 +2,27 @@
 if(isset($_POST)){
     header("Content-Type: text/plain; charset=UTF-8");
         $array = [
-            "nom" => $_POST['nom'],
-            "prenom" => $_POST['prenom'],
-            "adresse" => $_POST['adresse'],
-            "code_postal" => $_POST['code_postal'],
-            "ville" => $_POST['ville'],
-            "date_naissance" => $_POST['date_naissance'],
-            "telephone" => $_POST['telephone'],
-            "email" => $_POST['email'],
-            "club" => $_POST['club'],
-            "ufolep" => $_POST['ufolep'],];
-        $url = 'http://localhost:2901/Service1.svc/Insert';
-        $ch = curl_init($url);
+            "Nom" => $_POST['nom'],
+            "Prenom" => $_POST['prenom'],
+            "Adresse" => $_POST['adresse'],
+            "CodePostal" => $_POST['code_postal'],
+            "Ville" => $_POST['ville'],
+            "DateNaissance" => $_POST['date_naissance'],
+            "Telephone" => $_POST['telephone'],
+            "Email" => $_POST['email'],
+            "Club" => $_POST['club'],
+            "Ufolep" => $_POST['ufolep'],
+        ];
         $json = json_encode($array);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        $result = curl_exec($ch);
-    }
+        $url = 'http://localhost:2901/Service1.svc/Insert';
+        echo $json;
+
+        $opts = array('http' => array (
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $json
+            )
+        );
+        $context = stream_context_create($opts);
+        $result = file_get_contents($url, false, $context);
 ?>
