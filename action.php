9d5c5769957404fdfdb1a6ccce6e7074f36dc1a7
Monokame <jeremy.marc.pro@gmail.com>
2019-04-27 18:13:08 +0200

    Update button

--- action.php
+++ action.php
@@ -1,6 +1,5 @@
 <?php
 if(isset($_POST)){
-    header("Content-Type: text/plain; charset=UTF-8");
         $array = [
             "Nom" => $_POST['nom'],
             "Prenom" => $_POST['prenom'],
@@ -16,13 +15,13 @@ if(isset($_POST)){
         $json = json_encode($array);
         $url = 'http://localhost:2901/Service1.svc/Insert';
         echo $json;
-
         $opts = array('http' => array (
             'method'  => 'POST',
-            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'header'  => 'Content-type: text/plain',
             'content' => $json
             )
         );
         $context = stream_context_create($opts);
         $result = file_get_contents($url, false, $context);
+    }
 ?>
