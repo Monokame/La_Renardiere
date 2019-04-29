bc3affbfa63afe6e410e5d1e382a030c988234bf
Monokame <jeremy.marc.pro@gmail.com>
2019-04-29 23:03:25 +0200

    Add section animation

--- script.js
+++ script.js
@@ -2,6 +2,27 @@
 test.addEventListener('click',()=>{
 document.getElementById('inscription').style.display="block";
 })*/
-function test() {
-  document.getElementById('inscription').style.display="block";
+function test(){
+  document.getElementById('formInscription').style.display="none";
+  document.getElementById('result').style.display="none";
+  document.getElementById('formContact').style.display="none";
+  document.getElementById('home').style.display="block";
+}
+function test2() {
+  document.getElementById('home').style.display="none";
+  document.getElementById('result').style.display="none";
+  document.getElementById('formContact').style.display="none";
+  document.getElementById('formInscription').style.display="block";
+}
+function test3(){
+  document.getElementById('home').style.display="none";
+  document.getElementById('formInscription').style.display="none";
+  document.getElementById('formContact').style.display="none";
+  document.getElementById('result').style.display="block";
+}
+function test4(){
+  document.getElementById('home').style.display="none";
+  document.getElementById('formInscription').style.display="none";
+  document.getElementById('result').style.display="none";
+  document.getElementById('formContact').style.display="block";
 }
