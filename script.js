93a67ab15bf1c34d98c8a43ae74e990c76fc2945
Monokame <jeremy.marc.pro@gmail.com>
2019-04-29 19:07:42 +0200

    Test js

---
+++ script.js
@@ -0,0 +1,7 @@
+/*var test=document.getElementById('accueil');
+test.addEventListener('click',()=>{
+document.getElementById('inscription').style.display="block";
+})*/
+function test() {
+  document.getElementById('inscription').style.display="block";
+}
