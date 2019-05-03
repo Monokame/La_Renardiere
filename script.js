ed7e75a1e82c7046231fdfd16ecf6d486f2834d7
Adrien Maillard <adrien.mld@laposte.net>
2019-05-03 17:31:04 +0200

    ajout de la page contact (impossible d'écrire), ajout du footer dans le CSS et JS

--- script.js
+++ script.js
@@ -2,27 +2,40 @@
 test.addEventListener('click',()=>{
 document.getElementById('inscription').style.display="block";
 })*/
+
+//acceuil
 function test(){
   document.getElementById('formInscription').style.display="none";
   document.getElementById('result').style.display="none";
   document.getElementById('formContact').style.display="none";
   document.getElementById('home').style.display="block";
+  document.getElementById('footer').style.top='100%';
 }
+
+//inscription
 function test2() {
   document.getElementById('home').style.display="none";
   document.getElementById('result').style.display="none";
   document.getElementById('formContact').style.display="none";
   document.getElementById('formInscription').style.display="block";
+  document.getElementById('footer').style.top='0';
+  
 }
+
+//parcours
 function test3(){
   document.getElementById('home').style.display="none";
   document.getElementById('formInscription').style.display="none";
   document.getElementById('formContact').style.display="none";
   document.getElementById('result').style.display="block";
+  document.getElementById('footer').style.top='100%';
 }
+
+//contact
 function test4(){
   document.getElementById('home').style.display="none";
   document.getElementById('formInscription').style.display="none";
   document.getElementById('result').style.display="none";
   document.getElementById('formContact').style.display="block";
+  document.getElementById('footer').style.top='0';
 }
