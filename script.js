6db3010d9278f56226f0f110acbc9ee92254c97f
Adrien Maillard <adrien.mld@laposte.net>
2019-05-04 13:10:34 +0200

    modif fichier js et css, ajout d'un nouveau fichier parcours-info pour chaque clique sur un parcours différent

--- script.js
+++ script.js
@@ -1,40 +1,66 @@
-/*var test=document.getElementById('accueil');
-test.addEventListener('click',()=>{
-document.getElementById('inscription').style.display="block";
-})*/
 var accueil = document.getElementById('accueil');
 var inscription = document.getElementById('inscription');
 var parcours = document.getElementById('parcours');
 var contact = document.getElementById('contact');
 var titre = document.getElementById('titre');
 var footer = document.getElementById('footer');
+var img = document.getElementsByClassName('image');
 
 window.onload = () => {
 
   accueil.onclick = () => {
     document.location = 'index.html';
-    footer.style.top = '100%';
   };
 
   contact.onclick = () => {
     document.location = 'contact.html';
-    footer.style.top = '0';
   };
 
   inscription.onclick = () => {
     document.location = 'inscription.html';
-    footer.style.top = '0';
-    
-    
   };
 
   parcours.onclick = () => {
     document.location = 'parcours.html';
-    footer.style.top = '0%';
   };
 
   titre.onclick = () => {
     document.location = 'index.html';
-    footer.style.top = '100%';
   };
+};
+
+var tab = new Array();
+
+
+for (let i = 0; i < img.length; i++) {
+  img[i].onmouseover = () => {
+    
+    imageMouseOn(img[i]);
+  };
+  img[i].onmouseout = () => {
+    imageMouseOff(img[i]);
+  };
+  img[i].onmousedown = () => {
+    let source = img[i].getAttribute('src');
+    imageCreation(source);
+  };
+
+};
+
+function imageMouseOn(div) {
+  div.style.transform = 'scale(1.05)';
+  div.style.border = 'solid 2px #48dbfb';
+};
+
+function imageMouseOff(div) {
+  div.style.transform = 'scale(1)';
+  div.style.border = '';
+};
+
+function imageCreation(source) {
+  document.location = 'parcours-info.html';
+  let newImg = document.createElement('img');
+  newImg.src = 'img/test1.PNG';
+  newImg.className = 'image';
+  document.body.appendChild(newImg);
 };
